3fb27df5a62d0cf45ce86989a55f4d3b4a36c984
mitvoss <mitchellvos93@gmail.com>
2021-06-25 13:45:55 +0200

    loops.js af

--- loops.js
+++ loops.js
@@ -6,12 +6,10 @@
 // Hoera!
 // ==========================================
 
-/*
 for (let i = 0; i <3; i++ ){
     const hoera = 'hoera'
     console.log(hoera);
 }
-*/
 
 
 // ==========================================
@@ -23,13 +21,10 @@ for (let i = 0; i <3; i++ ){
 // loop...
 // klaar!
  // ==========================================
-const loop = 'loop...'
-const klaar = 'klaar'
-
 for (let ii = 0; ii<4; ii++){
-    console.log(loop);
+    console.log('loop');
 }
-    console.log(klaar);
+    console.log('klaar');
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -44,9 +39,13 @@ for (let ii = 0; ii<4; ii++){
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
 for (let i = 0; i<16; i++){
-    if console.log(i)
+    if (i % 2 === 0){
+        console.log((i) + ' is even!')
     }
-
+    if (i % 2 === 1){
+        console.log((i) + ' is oneven!')
+    }
+}
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
 // Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
@@ -62,4 +61,17 @@ for (let i = 0; i<16; i++){
 // >> 8
 // >> 9
 // ==========================================
+for (let i = 0; i < 10; i++){
+    if (i <= 2){
+        console.log(i)
+    }
 
+
+    if (i >= 3 && i < 6){
+        console.log('>'+(i))
+    }
+
+    if (i >= 6){
+        console.log('>>'+(i))
+    }
+}
